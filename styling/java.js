668a566c56f8b6560d49f97b726b23c7be4b34bf
Nokwanda15 <nokwanda.melissa15@gmail.com>
2024-09-21 23:51:28 +0200

    Added an API for weather search results

--- styling/java.js
+++ styling/java.js
@@ -1,8 +1,24 @@
+function showCityWeather(response) {
+  let temperature = document.querySelector("#temp-value");
+  let temp = response.data.temperature.current;
+  temperature.innerHTML = Math.round(temp);
+
+  let currentCity = document.querySelector("#city-name");
+  currentCity.innerHTML = response.data.city;
+}
+
+function enteredCity(name) {
+  let apiKey = `9ceba7ba7t6e28o34065e0b00b4cfe71`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${name}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(showCityWeather);
+}
+
 function submitted(event) {
   event.preventDefault();
   let box = document.querySelector("#searchbox");
-  let currentCity = document.querySelector("#city-name");
-  currentCity.innerHTML = box.value;
+
+  enteredCity(box.value);
 }
 
 let searchBoxElement = document.querySelector("#search-field");
