08dcb5959fc57c4951fbf7ca0a69039d28668326
Nokwanda15 <nokwanda.melissa15@gmail.com>
2024-09-25 23:41:31 +0200

    included more weather data from the API to my JS

--- styling/java.js
+++ styling/java.js
@@ -1,10 +1,45 @@
 function showCityWeather(response) {
+  console.log(response);
   let temperature = document.querySelector("#temp-value");
   let temp = response.data.temperature.current;
   temperature.innerHTML = Math.round(temp);
 
+  let conditionElement = document.querySelector("#weather-condition");
+  conditionElement.innerHTML = response.data.condition.description;
+
   let currentCity = document.querySelector("#city-name");
   currentCity.innerHTML = response.data.city;
+
+  let humidityElement = document.querySelector("#humid");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+
+  let windElement = document.querySelector("#windy");
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+
+  let currentDate = new Date(response.data.time * 1000);
+
+  let currentTime = document.querySelector("#time");
+  currentTime.innerHTML = formatDate(currentDate);
+}
+function formatDate(currentDate) {
+  let hours = currentDate.getHours();
+  let minutes = currentDate.getMinutes();
+  console.log(currentDate.getHours);
+
+  let week = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let today = week[currentDate.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${today} ${hours}:${minutes}`;
 }
 
 function enteredCity(name) {
